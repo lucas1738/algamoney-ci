6494b1076f412184cd3da05b1192bcfb108d1cb2
Your Name <lucasbarbosa1738@gmail.com>
2020-12-27 17:14:59 -0300

    integracao com travis-ci

---
+++ app/src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -0,0 +1,83 @@
+package com.example.algamoney.api.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "lancamento")
+public class Lancamento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotNull
+	private String descricao;
+
+	@NotNull
+	@Column(name = "data_vencimento")
+	private LocalDate dataVencimento;
+
+	@Column(name = "data_pagamento")
+	private LocalDate dataPagamento;
+
+	@NotNull
+	private BigDecimal valor;
+
+	private String observacao;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private TipoLancamento tipo;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "codigo_categoria")
+	private Categoria categoria;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "codigo_pessoa")
+	private Pessoa pessoa;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lancamento other = (Lancamento) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
